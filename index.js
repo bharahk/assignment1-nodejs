ece1f5ab2e6ec091b3ad23c891ca8b9e8420cc92
bharath kumar s <bharathkumar.s@niveussolutions.com>
2023-04-06 13:48:14 +0530

    creating the file using promises

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// github repository link: https://github.com/bharahk/assignment1-nodejs
+
+const fsPromise = require("fs").promises;
 
 // create a promoise to display a mesaage and cal the promise.
 const promise = new Promise((resolve, reject) => {
@@ -5,3 +8,12 @@ const promise = new Promise((resolve, reject) => {
 });
 
 promise.then(message => console.log(message));
+
+// Create a file using fs module and async/await.
+async function createFile(fileName, fileContents) {
+    await fsPromise.writeFile(fileName, fileContents);
+}
+
+createFile("test.txt", "This is test file.")
+.then(() => console.log("file created successfully."))
+.catch(err => console.log(err.toString()));
